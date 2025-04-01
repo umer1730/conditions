6be6bc38e96fd4195ce93d2e7cd35770ca9d738e
umer <umery1730@gmail.com>
2025-04-01 20:50:01 +0500

    add

---
+++ task13.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int x,y;
+    int z;
+    cout<<"Enter first num:";
+    cin>>x;
+    cout<<"Enter second num:";
+    cin>>y;
+    z = x + y;
+    cout<<"Enter the sum is:"<<z;
+    
+
+}
