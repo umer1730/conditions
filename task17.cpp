1d0146f6f4b8b6340386c438ea46b3669dfac757
umer <umery1730@gmail.com>
2025-04-03 22:58:28 +0500

    add

---
+++ task17.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string name;
+    int age;
+    cout<<"Enter the name:";
+    cin>>name;
+    cout<<"Enter the age:";
+    cin>>age;
+    cout<<name <<" " <<"is" <<" " <<age <<" " <<"years old." ;
+}
