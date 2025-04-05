9abd3758a205a01195d24d9f352f7d199c7feee3
umer <umery1730@gmail.com>
2025-04-05 15:08:10 +0500

    add

---
+++ task19.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int x,y;
+    cout<<"Enter the num:";
+    cin>>x;
+    cout<<"Enter the num:";
+    cin>>y;
+    int z = x + y;
+    cout<<"Total value is:"<<z;
+}
