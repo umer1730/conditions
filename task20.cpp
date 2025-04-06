c98ec0fc19b03d5718c4952f03fb6b3788025383
umer <umery1730@gmail.com>
2025-04-06 15:40:28 +0500

    addd

---
+++ task20.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int num1,num2;
+    cout<<"Enter the number:";
+    cin>>num1;
+    cout<<"Enter the number:";
+    cin>>num2;
+    int num3 = (num1 + num2) / 2;
+    cout<<"the value is:"<<num3;
+    
+}
