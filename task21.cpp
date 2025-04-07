55636971b66d7f95ba1496cb3dedd9c5cf3788ac
umer <umery1730@gmail.com>
2025-04-07 19:01:16 +0500

    add

---
+++ task21.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int num1,num2;
+    cout<<"Enter the num1:";
+    cin>>num1;
+    cout<<"Enter the num2:";
+    cin>>num2;
+    int num3 = num1 * num2;
+    cout<<"The total value is:"<<num3;
+}
