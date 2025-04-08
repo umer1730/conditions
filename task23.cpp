9d1dc63ea96cc0df564a800bdb417bedb59717fb
umer <umery1730@gmail.com>
2025-04-08 17:29:38 +0500

    adding

---
+++ task23.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string name;
+    int marks;
+    cout<<"Enter the name:";
+    cin>>name;
+    cout<<"Enter the marks:";
+    cin>>marks;
+    cout<<name <<" "<<"get"<<" "<<marks<<" "<<"out of 1100.";
+}
