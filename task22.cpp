41105cbaff85525aae9805473082adae9d192d18
umer <umery1730@gmail.com>
2025-04-08 17:22:14 +0500

    add

---
+++ task22.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int num1,num2,num3;
+    cout<<"Enter the num1:";
+    cin>>num1;
+    cout<<"Enter the num2:";
+    cin>>num2;
+    cout<<"Enter the num3:";
+    cin>>num3;
+    int num = (num1 + num2) - num3;
+    cout<<"Total num is:"<<num;
+}
