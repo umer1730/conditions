85de97799e7db7b09a4f04f0a739b843dc36c844
umer <umery1730@gmail.com>
2025-04-14 16:59:35 +0500

    add

---
+++ task38.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string name;
+    float time;
+    cout<<"Enter the name:";
+    cin>>name;
+    cout<<"Enter teh time:";
+    cin>>time;
+    cout<<name<<" "<<"completed task in"<<" "<<time;
+}
